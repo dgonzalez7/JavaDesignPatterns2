20a500b02d1035c9a55d276f9bc58d79b59012bc
Dave Gonzalez <dgonzalez@thosegonzos.com>
2020-03-09 12:59:51 -0500

    Add Class HelloWorld.java

---
+++ JavaDesignPatterns2/src/chapter01/HelloWorld.java
@@ -0,0 +1,13 @@
+package chapter01;
+
+public class HelloWorld 
+{
+
+	public static void main(String[] args) 
+	{
+		System.out.println("Hello World!");
+		
+		System.out.println("Java version: " + System.getProperty("java.version") + "\n\n");
+	}
+
+}
